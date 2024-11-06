e8b542b0dce30dd1cb7eccdc92f1240b85965794
Rigebs <riclarobr04@gmail.com>
2024-11-05 21:31:05 -0500

    test: deploy in vercel

--- frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  NG_APP_URL_API_AUTH: "https://sistema-reserva-futbol.onrender.com/auth",
-  NG_APP_URL_API_GENERAL: "https://sistema-reserva-futbol.onrender.com/api/v1",
-};
