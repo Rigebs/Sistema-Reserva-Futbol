ff2e8cdb0066032649130818bc4ea124e9e9a245
Rigebs <riclarobr04@gmail.com>
2024-11-05 21:21:53 -0500

    test: deploy in vercel

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    NG_APP_URL_API_AUTH: 'https://sistema-reserva-futbol.onrender.com/auth',
-    NG_APP_URL_API_GENERAL: 'https://sistema-reserva-futbol.onrender.com/api/v1',
+  production: false,
+  NG_APP_URL_API_AUTH: "https://sistema-reserva-futbol.onrender.com/auth",
+  NG_APP_URL_API_GENERAL: "https://sistema-reserva-futbol.onrender.com/api/v1",
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,2 +1,5 @@
 export const environment = {
+  production: true,
+  NG_APP_URL_API_AUTH: process.env["NG_APP_URL_API_AUTH"] || "",
+  NG_APP_URL_API_GENERAL: process.env["NG_APP_URL_API_GENERAL"] || "",
 };
