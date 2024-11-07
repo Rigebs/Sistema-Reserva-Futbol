c8831809ec0a31b87cf2b50c69b4227592b16d3c
Shujaa <jospers557@gmail.com>
2024-11-07 10:46:59 -0500

    ci: users api

--- backend/src/main/java/com/cruz_sur/api/controller/UserController.java
+++ backend/src/main/java/com/cruz_sur/api/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 @RestController
 @AllArgsConstructor
 public class UserController {
