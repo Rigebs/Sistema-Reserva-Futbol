ddb3cfbeaaa69901f165357724539d8b7960ce3c
codeShuja <jospers558@outlook.com>
2024-11-08 12:58:39 -0500

    test: release apis department, province and district

--- backend/src/main/java/com/cruz_sur/api/security/SecurityConfiguration.java
+++ backend/src/main/java/com/cruz_sur/api/security/SecurityConfiguration.java
@@ -32,8 +32,10 @@ public class SecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/api/v1/campos/with-sede").permitAll()
                         .requestMatchers("/api/v1/campos/available-sedes").permitAll()
+                        .requestMatchers("/api/v1/departamento").permitAll()
+                        .requestMatchers("/api/v1/distrito").permitAll()
+                        .requestMatchers("/api/v1/provincia").permitAll()
                         .requestMatchers("/api/v1/campos//usuario/{usuarioId}/with-sede").permitAll()
                         .requestMatchers("/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
